89ac98aae7d4070e1d2720bef85e03ac71be85b6
SiddheshRangnath <siddhesh.rangnath@gmail.com>
2017-03-29 13:00:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -41,6 +41,24 @@ app.post('/update', function(req, res) {
     });
 });
 
+app.post("/new_contact", function(req, res) {
+  var notification =req.body["soapenv:envelope"]["soapenv:body"][0]["notifications"][0];
+  var sessionId = notification["sessionid"][0];
+  var data = {};
+ 
+  if (notification["notification"] !== undefined) {
+    var sobject = notification["notification"][0]["sobject"][0];
+    Object.keys(sobject).forEach(function(key) {
+      if (key.indexOf("sf:") == 0) {
+        var newKey = key.substr(3);
+        data[newKey] = sobject[key][0];
+      }
+    });
+    // do something #awesome with the data and sessionId
+  }
+  res.status(201).end();
+});
+
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
